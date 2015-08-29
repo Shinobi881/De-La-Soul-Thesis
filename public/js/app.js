ff00cec9154a56bcebc67cc08679d2cf70e61a56
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-08-28 21:03:46 -0700

    Angular tutorial from the website while working with our code

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,17 @@
 var app = angular.module('soundGlomerate', []);
 
 app.controller('NavBarController', function(){
-  
+  // this initialized the tab to 1. This replaces ng-init in the html file
+    this.tab = 1;
+    // this replaces the "ng-click="tab = 1"" from the index.html, which assigns valuea or expression to a variable
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+
+    // This is checking to see if a tab in the nav bar is currently selected
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    }
 })
 
 })();
